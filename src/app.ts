46db4af4a01207f73d182173dc1bf7b160a99d5c
Joshijay1917 <jayjoshi1912007@gmail.com>
2026-07-25 15:17:12 +0530

    fix: parse body epxress in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express"
 import whatsAppRouter from "./routes/whatsapp.route.js"
 
-const app: any = express()
+const app: any = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/whatsapp', whatsAppRouter)
 
